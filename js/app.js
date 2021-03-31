c1a8e67ddc50b56e9a877fadff20c529194a4eca
Shawn Wood <Shawnwood97@gmail.com>
2021-03-30 22:25:41 -0600

    added comments, added another music file with a timeout

--- js/app.js
+++ js/app.js
@@ -1,28 +1,36 @@
-function mouseOverEvent(eventDetails) {
+// Function for mousover event
+function mouseOverEvent() {
   // console.log(eventDetails);
   h1Hover.style.background = "red";
   h1Hover.style.color = "purple";
   h1Hover.innerText = "ARE YOU TRIGGERED BY MY COLOR CHOICES?!";
 }
-
-function mouseClickEvent(eventDetails) {
+// function for click event
+function mouseClickEvent() {
   // console.log(eventDetails);
-  h2Click.innerText = "Be Careful..... :)";
+  h2Click.innerText = "Something cool will happen when you press spacebar.";
 }
-
+// function for spacebar press, plays music, one of them with a timeout.
+// does it make sense to have these variables inside?
 function spacePress(keyPress) {
   // console.log(eventDetails);
   let music = new Audio("/images/lol.mp3");
+  let music2 = new Audio("/images/lol2.mp3");
   let imageSpace = document.getElementById("image");
   if (keyPress.code === "Space") {
     imageSpace.style.display = "block";
     music.play();
+    setTimeout(function () {
+      music2.play();
+    }, 2000);
   }
 }
 
+// variables for hover and click events
 let h1Hover = document.getElementById("mouse_hover");
 let h2Click = document.getElementById("mouse_click");
 
+// event listeners that trigger the functions
 h1Hover.addEventListener("mouseover", mouseOverEvent);
 
 h2Click.addEventListener("click", mouseClickEvent);
